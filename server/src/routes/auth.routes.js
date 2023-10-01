4ddb4da6bdefc7191477439d332e973a12ffcef4
gerardmtz <gerartmartinez@gmail.com>
2023-10-01 14:56:47 -0600

    Commit a fecha de Oct, 1 2023.
        Resumen de los cambios realizados:
    	modified:   server/README.md
    	modified:   server/package.json
    	modified:   server/src/config/database.config.js
    	new file:   server/src/controllers/users.controller.js
    	modified:   server/src/database.js
    	modified:   server/src/index.js
    	deleted:    server/src/models/user.model.js
    	modified:   server/src/passport/local.auth.js
    	new file:   server/src/routes/auth.routes.js
    	modified:   server/src/routes/index.routes.js
    	new file:   server/utilities/encryption.js
    	modified:   views/layouts/main.ejs
    	modified:   views/signin.ejs
    	modified:   views/signup.ejs

---
+++ server/src/routes/auth.routes.js
@@ -0,0 +1,44 @@
+// Módulo auth.routes.js encargado
+// de gestionar las peticiones y
+// enrutamiento hacia el servidor
+// para la autenticación y registro
+// del usuario.
+
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+// Rutas de manejo de sesión
+router.get('/auth/signup', (req, res, next) => {
+    res.render('signup');
+});
+
+router.post('/auth/signup', passport.authenticate('local-signup', {
+    successRedirect: '/auth/profile',
+    failureRedirect: '/auth/signup',
+    passReqToCallback: true
+}));
+
+
+router.get('/auth/signin', (req, res, next) => {
+    res.render('signin');
+});
+
+router.post('/auth/signin', passport.authenticate('local-signin', {
+    successRedirect: '/auth/profile',
+    failureRedirect: '/auth/signin',
+    passReqToCallback: true
+}));
+
+router.get('/auth/logout', (req, res, next) => {
+    req.logout((err) => {
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
+});
+
+router.get('/auth/profile', (req, res, next) => {
+    res.render('profile');
+});
+
+module.exports = router;
